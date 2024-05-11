d2b2776f70c91b6cfb08870982aadbeb9b7758fd
unknown <brandenek03912@hotmail.com>
2024-05-11 17:53:01 -0400

    core: implement loggers

--- Basalt.Framework.Logging/Standard/ConsoleLogger.cs
+++ Basalt.Framework.Logging/Standard/ConsoleLogger.cs
@@ -1,25 +1,51 @@
-﻿
+﻿using System.Runtime.InteropServices;
+
 namespace Basalt.Framework.Logging.Standard;
 
+/// <summary>
+/// Logs messages to a console window
+/// </summary>
 public class ConsoleLogger : ILogger
 {
+    /// <summary>
+    /// Creates a new console logger
+    /// </summary>
+    public ConsoleLogger(bool requiresAlloc)
+    {
+        if (requiresAlloc)
+            AttachConsole(-1);
+    }
+
+    /// <inheritdoc/>
     public void Info(object message)
     {
-        throw new NotImplementedException();
+        LogToConsole(message, ConsoleColor.White);
     }
 
+    /// <inheritdoc/>
     public void Warn(object message)
     {
-        throw new NotImplementedException();
+        LogToConsole(message, ConsoleColor.Yellow);
     }
 
+    /// <inheritdoc/>
     public void Error(object message)
     {
-        throw new NotImplementedException();
+        LogToConsole(message, ConsoleColor.Red);
     }
 
+    /// <inheritdoc/>
     public void Debug(object message)
     {
-        throw new NotImplementedException();
+        LogToConsole(message, ConsoleColor.Cyan);
     }
+
+    private void LogToConsole(object message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+    }
+
+    [DllImport("kernel32", SetLastError = true)]
+    static extern bool AttachConsole(int dwProcessId);
 }
--- Basalt.Framework.Logging/Standard/FileLogger.cs
+++ Basalt.Framework.Logging/Standard/FileLogger.cs
@@ -1,25 +1,60 @@
 ﻿
 namespace Basalt.Framework.Logging.Standard;
 
+/// <summary>
+/// Logs messages to a 'Latest.log' file
+/// </summary>
 public class FileLogger : ILogger
 {
+    private readonly string _path;
+
+    /// <summary>
+    /// Creates a new file logger
+    /// </summary>
+    public FileLogger(string directory)
+    {
+        Directory.CreateDirectory(directory);
+        _path = Path.Combine(directory, "Latest.log");
+
+        if (File.Exists(_path))
+            File.SetAttributes(_path, File.GetAttributes(_path) & ~FileAttributes.ReadOnly);
+
+        File.WriteAllText(_path, string.Empty);
+        File.SetAttributes(_path, FileAttributes.ReadOnly);
+    }
+
+    /// <inheritdoc/>
     public void Info(object message)
     {
-        throw new NotImplementedException();
+        LogtoFile(message, "Info");
     }
 
+    /// <inheritdoc/>
     public void Warn(object message)
     {
-        throw new NotImplementedException();
+        LogtoFile(message, "Warning");
     }
 
+    /// <inheritdoc/>
     public void Error(object message)
     {
-        throw new NotImplementedException();
+        LogtoFile(message, "Error");
     }
 
+    /// <inheritdoc/>
     public void Debug(object message)
     {
-        throw new NotImplementedException();
+        LogtoFile(message, "Debug");
+    }
+
+    private void LogtoFile(object message, string type)
+    {
+        string output = $"[{DateTime.Now:HH:mm:ss} {type}] {message}{Environment.NewLine}";
+
+        if (File.Exists(_path))
+            File.SetAttributes(_path, File.GetAttributes(_path) & ~FileAttributes.ReadOnly);
+
+        File.AppendAllText(_path, output);
+        File.SetAttributes(_path, FileAttributes.ReadOnly);
     }
 }
