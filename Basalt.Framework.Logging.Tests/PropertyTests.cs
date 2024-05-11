41383262d2980986f0be55a7ff60ae5ea743f3f2
unknown <brandenek03912@hotmail.com>
2024-05-11 18:37:08 -0400

    tests: property methods

---
+++ Basalt.Framework.Logging.Tests/PropertyTests.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Basalt.Framework.Logging.Tests;
+
+[TestClass]
+public class PropertyTests
+{
+    private TestLogger _logger = new();
+
+    [TestInitialize]
+    public void TestReset()
+    {
+        Logger.Reset();
+        _logger = new TestLogger();
+    }
+
+    [TestMethod]
+    public void Properties_Empty()
+    {
+        Logger.Initialize(new ILogger[] { _logger });
+
+        Logger.Debug("Test");
+        Assert.AreEqual(1, _logger.DebugMessages);
+    }
+
+    [TestMethod]
+    public void Properties_DisplayDebug()
+    {
+        Logger.Initialize(new ILogger[] { _logger }, new Properties()
+        {
+            DisplayDebug = false
+        });
+
+        Logger.Debug("Test");
+        Assert.AreEqual(0, _logger.DebugMessages);
+    }
+}
---
+++ Basalt.Framework.Logging.Tests/TestLogger.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Basalt.Framework.Logging.Tests;
+
+public class TestLogger : ILogger
+{
+    public int InfoMessages { get; private set; } = 0;
+    public int WarningMessages { get; private set; } = 0;
+    public int ErrorMessages { get; private set; } = 0;
+    public int DebugMessages { get; private set; } = 0;
+
+    public void Info(object message) => InfoMessages++;
+
+    public void Warn(object message) => WarningMessages++;
+
+    public void Error(object message) => ErrorMessages++;
+
+    public void Debug(object message) => DebugMessages++;
+}
