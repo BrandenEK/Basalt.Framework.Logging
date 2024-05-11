a227c6f8c52c1ce7ecd07cb1f71e6375bc0a3bb7
unknown <brandenek03912@hotmail.com>
2024-05-11 18:33:02 -0400

    tests: init methods

--- Basalt.Framework.Logging.Tests/InitializationTests.cs
+++ Basalt.Framework.Logging.Tests/InitializationTests.cs
@@ -4,14 +4,17 @@ namespace Basalt.Framework.Logging.Tests;
 [TestClass]
 public class InitializationTests
 {
+    private TestLogger _logger = new();
+
     [TestInitialize]
-    public void Initialize()
+    public void TestReset()
     {
         Logger.Reset();
+        _logger = new TestLogger();
     }
 
     [TestMethod]
-    public void NoInitialize()
+    public void Initialize_Never()
     {
         Assert.ThrowsException<InvalidOperationException>(() => Logger.Info("Test"));
     }
@@ -25,10 +28,34 @@ public class InitializationTests
     }
 
     [TestMethod]
-    public void Initialize_Empty()
+    public void Initialize_Zero()
     {
         Logger.Initialize(Array.Empty<ILogger>());
 
         Logger.Info("Test");
+    }
+
+    [TestMethod]
+    public void Initialize_One()
+    {
+        Logger.Initialize(new ILogger[]
+        {
+            _logger
+        });
+
+        Logger.Info("Test");
+        Assert.AreEqual(1, _logger.InfoMessages);
+    }
+
+    [TestMethod]
+    public void Initialize_Two()
+    {
+        Logger.Initialize(new ILogger[]
+        {
+            _logger, _logger
+        });
+
+        Logger.Info("Test");
+        Assert.AreEqual(2, _logger.InfoMessages);
     }
 }
